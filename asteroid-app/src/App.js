dfcbacbb24c3001fd7a573f8914861d127cb7bf4
Guernsey219 <hrylee@ucdavis.edu>
2021-12-10 14:50:12 -0800

    added search bars

--- asteroid-app/src/App.js
+++ asteroid-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Container from './Container';
 import TitleBar from "./TitleBar.js";
 
 const App = () => {
   return (
     <div className="App">
       <TitleBar />
+      <Container />
     </div>
   );
 }
